36503c9c952b61328a011c60bb4d1f29f8a1d02d
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-08-13 18:25:19 +0300

    feat: Random generator inline functions

---
+++ src/random-gen.h
@@ -0,0 +1,29 @@
+#include <random>
+#include <type_traits>
+#include <vector>
+
+template <typename T>
+inline typename std::enable_if<std::is_integral<T>::value, T>::type get_random(T min, T max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(min, max);
+    return distr(gen);
+}
+
+template <typename T>
+inline typename std::enable_if<std::is_floating_point<T>::value, T>::type get_random(T min, T max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min, max);
+    return dis(gen);
+}
+
+template <typename T>
+inline T get_random(std::initializer_list<T> list) {
+    std::vector<T> vec(list);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<std::size_t> distr(0, vec.size() - 1);
+    return vec[distr(gen)];
+}
