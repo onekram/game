823a15dab8f85525dc8fae23b9adb8a74f5d9ac5
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-08-13 18:25:57 +0300

    feat: Random moving circles

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
+#include "random-gen.h"
+
 #include <flecs.h>
 #include <raylib.h>
 
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 
-const int screenWidth = 800;
-const int screenHeight = 600;
+const uint32_t screen_width = 800;
+const uint32_t screen_height = 600;
 
 struct velocity {
-    float x, y;
+    double x, y;
 
+    static velocity generate_random_velocity() {
+        return {get_random({-0.01, 0.01}), get_random({-0.01, 0.01})};
+    };
+
+    friend std::ostream& operator<<(std::ostream& s, const velocity& v) {
+        s << "Velocity: " << v.x << "  " << v.y;
+        return s;
+    }
 };
 
 struct position {
-    float x, y;
+    double x, y;
 
     template <typename Function>
     void move(const velocity& v, Function func) {
@@ -23,6 +34,15 @@ struct position {
 
     void move_vector(const velocity& v) {
         move(v, std::plus<>());
+    }
+
+    static position generate_random_position(double MIN_X, double MAX_X, double MIN_Y, double MAX_Y) {
+        return {get_random(MIN_X, MAX_X), get_random(MIN_Y, MAX_Y)};
+    }
+
+    friend std::ostream& operator<<(std::ostream& s, const position& p) {
+        s << "Position: " << p.x << "  " << p.y;
+        return s;
     }
 };
 
@@ -36,23 +56,44 @@ void move_all(const flecs::world& world, Function func) {
 }
 
 void init() {
-    InitWindow(screenWidth, screenHeight, "Flecs and Raylib Example");
+    InitWindow(screen_width, screen_height, "Flecs and Raylib Example");
 }
 
 void init_entities(const flecs::world& world, std::size_t count = 1) {
     for (std::size_t i = 0; i < count; ++i) {
-        auto e = world.entity();
+        world.entity()
+            .set<position>(position::generate_random_position(0, screen_width, 0, screen_height))
+            .set<velocity>(velocity::generate_random_velocity());
     }
-    world.each([](flecs::entity& e) {
-        e.set<position>({10, 20});
-        e.set<velocity>({1, 1});
+}
+
+void print_entities(const flecs::world& world) {
+    world.each([](position& p, velocity& v) { std::cout << p << '\n' << v << "\n\n"; });
+}
+
+void draw_entities(const flecs::world& world) {
+    world.each([](position& p) {
+        DrawCircle(static_cast<int32_t>(std::round(p.x)), static_cast<int32_t>(std::round(p.y)), 10.0, DARKBLUE);
     });
 }
 
-// Render system
-void render(ecs_iter_t* it) {}
+void drow(const flecs::world& world) {
+    init();
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        draw_entities(world);
+        move_all(world);
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
 
 int main() {
-    // init();
     flecs::world world;
+    init_entities(world, 10);
+//    print_entities(world);
+    drow(world);
 }
