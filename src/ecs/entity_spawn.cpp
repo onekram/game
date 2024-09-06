53cf35066e704f400eec87ae708e74b784848030
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-09-06 19:34:44 +0300

    style: Delete useless lib

--- src/ecs/entity_spawn.cpp
+++ src/ecs/entity_spawn.cpp
@@ -7,8 +7,6 @@
 #include "sounds.h"
 #include "textures.h"
 
-#include <iostream>
-
 auto entity_spawn::enemy_spawn_system_factory(std::size_t count) {
     return [count](flecs::iter& it) {
         for (std::size_t i = 0; i < count; ++i) {
